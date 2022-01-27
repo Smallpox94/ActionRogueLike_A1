ec7e45f8f9e50e3a0d42c243d52deb2210738840
Smallpox94 <courtois.sebastien@gmail.com>
2022-01-26 23:22:54 -0500

    Initial Commit + Extra Content

---
+++ Source/ActionRogueLike_A1/ActionRogueLike_A1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ActionRogueLike_A1GameModeBase.h"
+
---
+++ Source/ActionRogueLike_A1/ActionRogueLike_A1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueLike_A1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_A1_API AActionRogueLike_A1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
